32ae1a28ad45617cd1a00a18b60b11f196ad69e0
moaz <moaznabil886@gmail.com>
2022-05-26 00:04:05 +0200

    MapReduce write to a parquet file

--- src/main/java/com/university/bigdata/milestone2/MapReduce.java
+++ src/main/java/com/university/bigdata/milestone2/MapReduce.java
@@ -1,5 +1,4 @@
 package com.university.bigdata.milestone2;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -11,14 +10,39 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.shaded.com.google.gson.Gson;
-
 import java.io.IOException;
-
 import static java.lang.Math.max;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.parquet.avro.AvroParquetOutputFormat;
+import org.apache.parquet.example.data.Group;
 
-public class MapReduce {
+public class MapReduce extends Configured implements Tool{
 
-    static long start, end;
+    static Map<String,GenericRecord> mp=new HashMap<>();
+
+    /// Schema
+    private static Schema schema ;
+    // Method to parse the schema
+    private static Schema parseSchema() {
+        Schema.Parser parser = new	Schema.Parser();
+        Schema schema = null;
+        try {
+            // Path to schema file
+            schema = parser.parse(new File("src/main/java/com/university/bigdata/milestone3/schema.avsc"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return schema;
+    }
 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, DoubleWritable>{
 
@@ -32,24 +56,28 @@ public class MapReduce {
                 message=message.concat("}}");
                 Info currentInfo = parser.fromJson(message, Info.class);
 
-                //currentInfo.Timestamp
-
-                if(currentInfo.Timestamp < start || currentInfo.Timestamp > end)
-                    continue;
                 long time=(currentInfo.Timestamp - 1647938697)/60;
                 //CPU utilization
-                word.set(currentInfo.serviceName+"_CPU_"+time);
+                word.set(currentInfo.serviceName+"_"+time+"_CPU");
                 context.write(word, new DoubleWritable(currentInfo.CPU));
-
-                word.set(currentInfo.serviceName+"_CPU_MAX_"+time);
+                //CPU MAX utilization
+                word.set(currentInfo.serviceName+"_"+time+"_CPU_MAX");
                 context.write(word, new DoubleWritable(currentInfo.CPU));
 
                 //Disk utilization
-                word.set(currentInfo.serviceName+"_DISK_"+time);
+                word.set(currentInfo.serviceName+"_"+time+"_DISK");
                 context.write(word, new DoubleWritable(currentInfo.Disk.Free/currentInfo.Disk.Total));
+                //Disk MAX utilization
+                word.set(currentInfo.serviceName+"_"+time+"_DISK_MAX");
+                context.write(word, new DoubleWritable(currentInfo.Disk.Free/currentInfo.Disk.Total));
+
                 // RAM utilization
-                word.set(currentInfo.serviceName+"_RAM_"+time);
+                word.set(currentInfo.serviceName+"_"+time+"_RAM");
                 context.write(word, new DoubleWritable(currentInfo.RAM.Free/currentInfo.RAM.Total));
+                // RAM MAX utilization
+                word.set(currentInfo.serviceName+"_"+time+"_RAM_MAX");
+                context.write(word, new DoubleWritable(currentInfo.RAM.Free/currentInfo.RAM.Total));
+
                 // count of health messages for each service
                 word.set(currentInfo.serviceName+"_"+time);
                 context.write(word, one);
@@ -57,12 +85,13 @@ public class MapReduce {
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable> {
-        private DoubleWritable result = new DoubleWritable();
+    public static class IntSumReducer extends Reducer<Text,DoubleWritable,Void,GenericRecord> {
+
+        private	GenericRecord record = new GenericData.Record(schema);
 
         public void reduce(Text key, Iterable<DoubleWritable> values, Context context ) throws IOException, InterruptedException {
-            Double ans = 0.0;
-            if(key.toString().length() <15 || key.toString().charAt(14)!= 'M')
+            double ans = 0.0;
+            if(!key.toString().contains("MAX"))
                 for (DoubleWritable val : values) {
                     ans += val.get();
                 }
@@ -70,40 +99,78 @@ public class MapReduce {
                 for (DoubleWritable val : values) {
                     ans = max(val.get(),ans);
                 }
-            result.set(ans);
 
-            System.out.println("---------->" + key.toString());
+            String[] keys=key.toString().split("_");
+            String KEY= keys[0]+"_"+keys[1] ;
 
-            context.write(key, result);
+
+            mp.putIfAbsent(KEY,new GenericData.Record(schema));
+
+            if(keys.length == 2){
+                mp.get(KEY).put("count", ans);
+                mp.get(KEY).put("service", keys[0].charAt(8)-'0');
+                mp.get(KEY).put("time", Integer.parseInt(keys[1]));
+            }
+            else if(keys.length == 3){
+                mp.get(KEY).put(keys[2], ans);
+            }
+            else{
+                mp.get(KEY).put(keys[2]+"_"+keys[3], ans);
+            }
+            if(!mp.get(KEY).toString().contains("null")){
+                context.write(null, mp.get(KEY));
+                //System.out.println("++++++++++++++++" + mp.get(KEY).toString());
+            }
+            //System.out.println("---------->" + key.toString());
+            //System.out.println("============" + mp.get(KEY).toString());
+
         }
     }
 
     public static void main(String[] args) throws Exception {
+        schema = parseSchema();
+        int exitFlag = ToolRunner.run(new MapReduce(), args);
+        System.exit(exitFlag);
+    }
 
-        System.out.println(args[0]);
-        start = Long.parseLong(args[0]);
-        end = Long.parseLong(args[1]);
+    @Override
+    public int run(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        
+/*
         conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
         Path coreSite = new Path("/usr/local/hadoop/etc/hadoop/core-site.xml");
         Path hdfsSite = new Path("/usr/local/hadoop/etc/hadoop/hdfs-site.xml");
         conf.addResource(coreSite);
         conf.addResource(hdfsSite);
-
+*/
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(MapReduce.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
+        //job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(DoubleWritable.class);
-        FileInputFormat.addInputPath(job, new Path("/health_data/health_0.json"));
-        Path outputPath = new Path("/health_out");
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(DoubleWritable.class);
+
+
+
+        job.setOutputKeyClass(Void.class);
+        job.setOutputValueClass(Group.class);
+
+        job.setOutputFormatClass(AvroParquetOutputFormat.class);
+
+        // setting schema to be used
+        AvroParquetOutputFormat.setSchema(job, schema);
+
+
+
+        FileInputFormat.addInputPath(job, new Path("/home/moaz/health.txt"));
+        Path outputPath = new Path("/home/moaz/Downloads/testout");
         FileSystem fileSystem = FileSystem.get(conf);
         if(fileSystem.exists(outputPath))
             fileSystem.delete(outputPath);
         FileOutputFormat.setOutputPath(job, outputPath);
         job.waitForCompletion(true);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 }
