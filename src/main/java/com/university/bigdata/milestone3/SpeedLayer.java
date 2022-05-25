98832fdd8ba0b373ec92056524baa3aaa5e739ba
Louai Zahran <louai0nasr@gmail.com>
2022-05-25 23:56:34 +0200

    Spark tokenizer

--- src/main/java/com/university/bigdata/milestone3/SpeedLayer.java
+++ src/main/java/com/university/bigdata/milestone3/SpeedLayer.java
@@ -1,23 +1,54 @@
 package com.university.bigdata.milestone3;
 
+import com.google.gson.Gson;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class SpeedLayer implements Obs{
+
+    private static String[] tokenize(String line){
+        List<String> ret = new ArrayList<>();
+        Gson parser = new Gson();
+        String[] messages = line.toString().split("}}");
+        for (String message : messages) {
+            message = message.concat("}}");
+            ret.add(message);
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    private static String identity(String x){
+        return x;
+    }
+
     private static void wordCount(String fileName) {
-        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JD Word Counter");
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("Speed Layer");
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-        JavaRDD<String> inputFile = sparkContext.textFile(fileName);
-        JavaPairRDD<String, Integer> counts = inputFile
-                .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
-                .mapToPair(word -> new Tuple2<>(word, 1))
-                .reduceByKey((a, b) -> a + b);
-        counts.saveAsTextFile("CountData");
+        JavaRDD<String> objs = sparkContext.textFile(fileName);
+
+        objs.collect();
+        JavaRDD<String> objs2 = objs.map(new Function<String, String[]>() {
+            @Override
+            public String[] call(String s) throws Exception {
+                return tokenize(s);
+            }
+        }).flatMap(new FlatMapFunction<String[], String>() {
+            @Override
+            public Iterator<String> call(String[] stringStream) throws Exception {
+                return Arrays.stream(stringStream).iterator();
+            }
+        });
+
+        objs2.collect().forEach(System.out::println);
+
     }
 
     public static void main(String[] args) {
