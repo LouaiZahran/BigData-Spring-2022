5b53a22690364c4c2f093683b260d5f90d95e7ac
Louai Zahran <louai0nasr@gmail.com>
2022-05-26 15:47:48 +0200

    Refactored SpeedLayer and put the *_MAX as percentages

--- src/main/java/com/university/bigdata/milestone3/SpeedLayer.java
+++ src/main/java/com/university/bigdata/milestone3/SpeedLayer.java
@@ -3,27 +3,19 @@ package com.university.bigdata.milestone3;
 import com.google.gson.Gson;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.parquet.avro.AvroParquetWriter;
-import org.apache.parquet.hadoop.ParquetWriter;
-import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 public class SpeedLayer implements Obs{
 
-    static Schema schema;
-    static HashMap<String, GenericData.Record> records = new HashMap<>();
     static Gson parser = new Gson();
+    static Schema schema = ParquetWriter.parseSchema();
+    static HashMap<String, GenericData.Record> records = new HashMap<>();
 
     private static String[] tokenize(String line){
         List<String> ret = new ArrayList<>();
@@ -42,10 +34,10 @@ public class SpeedLayer implements Obs{
         ret.add(new Tuple2<>(name + "_" + time + "_Count", "1"));
         ret.add(new Tuple2<>(name + "_" + time + "_CPU", String.valueOf(info.CPU)));
         ret.add(new Tuple2<>(name + "_" + time + "_CPU_MAX", String.valueOf(info.CPU)));
-        ret.add(new Tuple2<>(name + "_" + time + "_Disk", String.valueOf(info.Disk.Free)));
-        ret.add(new Tuple2<>(name + "_" + time + "_Disk_MAX", String.valueOf(info.Disk.Free)));
-        ret.add(new Tuple2<>(name + "_" + time + "_RAM", String.valueOf(info.RAM.Free)));
-        ret.add(new Tuple2<>(name + "_" + time + "_RAM_MAX", String.valueOf(info.RAM.Free)));
+        ret.add(new Tuple2<>(name + "_" + time + "_Disk", String.valueOf(info.Disk.Free/info.Disk.Total)));
+        ret.add(new Tuple2<>(name + "_" + time + "_Disk_MAX", String.valueOf(info.Disk.Free/info.Disk.Total)));
+        ret.add(new Tuple2<>(name + "_" + time + "_RAM", String.valueOf(info.RAM.Free/info.RAM.Total)));
+        ret.add(new Tuple2<>(name + "_" + time + "_RAM_MAX", String.valueOf(info.RAM.Free/info.RAM.Total)));
         return ret.iterator();
     }
 
@@ -61,54 +53,6 @@ public class SpeedLayer implements Obs{
         double value2D = Double.parseDouble(value2);
         double ret = Math.max(value1D, value2D);
         return String.valueOf(ret);
-    }
-
-    private static void writeToParquetFile(List<GenericData.Record> recordList, Schema schema) {
-        // Output path for Parquet file in HDFS
-        Path path =	new	Path("data.parquet");
-        ParquetWriter<GenericData.Record> writer = null;
-        // Creating ParquetWriter using builder
-        try {
-            writer = AvroParquetWriter.
-                    <GenericData.Record>builder(path)
-                    .withRowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
-                    .withPageSize(ParquetWriter.DEFAULT_PAGE_SIZE)
-                    .withSchema(schema)
-                    .withConf(new Configuration())
-                    .withCompressionCodec(CompressionCodecName.SNAPPY)
-                    .withValidation(false)
-                    .withDictionaryEncoding(false)
-                    .build();
-            // writing records
-            for (GenericData.Record record : recordList) {
-                System.out.println(record);
-                writer.write(record);
-            }
-        }catch(IOException e) {
-            e.printStackTrace();
-        }finally {
-            if(writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private static Schema parseSchema() {
-        Schema.Parser parser = new	Schema.Parser();
-        Schema schema = null;
-        try {
-            // Path to schema file
-            schema = parser.parse(new File("src/main/java/com/university/bigdata/milestone3/schema.avsc"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return schema;
     }
 
     private static void addRecord(Tuple2<String, String> record){
@@ -141,8 +85,7 @@ public class SpeedLayer implements Obs{
         records.put(key, record1);
     }
 
-    private static void wordCount(String fileName) {
-        schema = parseSchema();
+    private static void launchSpark(String fileName) {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("Speed Layer");
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
         JavaRDD<String> messages = sparkContext.textFile(fileName);
@@ -157,10 +100,7 @@ public class SpeedLayer implements Obs{
 
         List<GenericData.Record> recordList = new ArrayList<>(records.values());
 
-        writeToParquetFile(recordList, schema);
-
-        //reducedRDD.collect().forEach(tuple -> System.out.println(tuple._1 + " " + tuple._2));
-        //reducedMaxRDD.collect().forEach(tuple -> System.out.println(tuple._1 + " " + tuple._2));
+        ParquetWriter.writeToParquetFile(recordList, schema);
     }
 
     public static void main(String[] args) {
@@ -170,7 +110,7 @@ public class SpeedLayer implements Obs{
             System.exit(0);
         }
 
-        wordCount(args[0]);
+        launchSpark(args[0]);
     }
 
     @Override
